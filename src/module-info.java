74df73e612cd9c323c45baaaaf4f8407a35fa6ce
saltishDev <saltishdev@gmail.com>
2021-10-29 15:47:46 +0900

    불필요한 폴더 및 파일 삭제

--- src/module-info.java
+++
@@ -1,2 +0,0 @@
-module github_practice {
-}
--- src/test_package/TestClass.java
+++
@@ -1,9 +0,0 @@
-package test_package;
-
-public class TestClass {
-	
-	public static void main(String[] args) {
-		System.out.println("Hello! This is Github Practicing!!");
-		
-	}
-}
